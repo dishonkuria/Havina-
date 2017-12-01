93f664a398636632e90cd8fc85e29405adc53839
dishonkuria <amster2ent@gmail.com>
2017-12-01 12:11:34 +0300

    add a function to paragraph and an alert

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ jQuery(document).ready(function() {
   jQuery("h1").click(function() {
     alert("This is a header.");
   });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
